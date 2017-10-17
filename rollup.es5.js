597d6c675c79efc1988624b7d523e3c49f2f5b2a
Patrick Martin <bypatrickmartin@gmail.com>
2017-10-16 20:49:14 -0700

    Added build scripts

---
+++ rollup.es5.js
@@ -0,0 +1,7 @@
+
+export default {
+    entry: './package/index.js',
+    format: 'es',
+    dest: './package/xmat.es5.js',
+    sourceMap: true
+  };
---
+++ rollup.umd.js
@@ -0,0 +1,8 @@
+
+export default {
+    entry: './package/index.js',
+    format: 'umd',
+    moduleName: 'xmat',
+    dest: './package/xmat.umd.js',
+    sourceMap: true
+};
